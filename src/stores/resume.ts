24cc8de53ee025419a4bb10a6b35369aef5206ba
Benjent <Benjent@hotmail.fr>
2024-03-10 22:41:30 +0100

    feat: allow work experience edition

--- src/stores/resume.ts
+++ src/stores/resume.ts
@@ -13,7 +13,8 @@ export const useResumeStore = defineStore("resume", () => {
     const linkedIn = ref("")
     const gitHub = ref("")
     const website = ref("")
-    const workingDays = ref([1, 2, 3, 4, 5])
 
-    return { address, drivingLicense, email, gitHub, linkedIn, name, phone, title, website }
+    const workExperience = ref([])
+
+    return { address, drivingLicense, email, gitHub, linkedIn, name, phone, title, website, workExperience }
 })
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export interface WorkExperience {
+    position?: string,
+    company?: string,
+    period?: string,
+    location?: string,
+    description?: string,
+    tasks?: Array<string>,
+}
