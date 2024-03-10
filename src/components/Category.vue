1f6404786486b321175ea99725190617184c6879
Benjent <Benjent@hotmail.fr>
2024-03-10 21:52:43 +0100

    feat: allow personal details edition

---
+++ src/components/Category.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main class="category">
+        <slot />
+    </main>
+</template>
+
+<style scoped>
+.category {
+    display: flex;
+    flex-direction: column;
+}
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+import { storeToRefs } from "pinia"
+import { useResumeStore } from "../stores/resume"
+import Category from './Category.vue'
+
+// defineProps<{ msg: string }>()
+
+const { address, drivingLicense, email, gitHub, linkedIn, name, phone, title, website } = storeToRefs(useResumeStore())
+
+// const count = ref(0)
+</script>
+
+<template>
+    <main class="editor">
+        <Category>
+            <label>
+                Name
+                <input v-model="name" />
+            </label>
+            <label>
+                Email
+                <input v-model="email" />
+            </label>
+            <label>
+                Phone
+                <input v-model="phone" />
+            </label>
+            <label>
+                Address
+                <input v-model="address" />
+            </label>
+            <label>
+                LinkedIn
+                <input v-model="linkedIn" />
+            </label>
+            <label>
+                GitHub
+                <input v-model="gitHub" />
+            </label>
+            <label>
+                Website
+                <input v-model="website" />
+            </label>
+            <label>
+                Driving license
+                <input v-model="drivingLicense" />
+            </label>
+            <label>
+                Title
+                <input v-model="title" />
+            </label>
+        </Category>
+    </main>
+</template>
+
+<style scoped>
+.editor {
+}
+</style>
