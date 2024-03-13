7e23125313fc801228c6c7312a64fede1b61d0ac
Pierre Charpentier <charpentier.pierre@outlook.com>
2024-03-13 22:19:52 +0100

    fix: typescript issues

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,16 @@
 export interface WorkExperience {
-    position?: string,
-    company?: string,
-    period?: string,
-    location?: string,
-    description?: string,
-    tasks?: Array<string>,
+  position: string;
+  company: string;
+  period: string;
+  location: string;
+  description: string;
+  tasks: Array<string>;
 }
 
 export interface Education {
-    diploma?: string,
-    institution?: string,
-    period?: string,
-    location?: string,
-    description?: string,
+  diploma: string;
+  institution: string;
+  period: string;
+  location: string;
+  description: string;
 }
