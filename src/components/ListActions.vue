d04515b587d987330670282606ecd40a08bd1073
Benjent <Benjent@hotmail.fr>
2024-03-13 23:00:46 +0100

    feat: allow up and down move of work experience and education

---
+++ src/components/ListActions.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineProps } from "vue"
+
+defineProps<{ index: number, listLength: number }>()
+</script>
+
+<template>
+    <div class="listActions">
+        <button v-if="listLength > 1 && index > 0" class="listActions__remove" @click="$emit('moveUp')">Move up</button>
+        <button v-if="listLength > 1 && index < listLength - 1" class="listActions__remove" @click="$emit('moveDown')">Move down</button>
+        <button class="listActions__remove" @click="$emit('remove')">Remove</button>
+    </div>
+</template>
+
+<style scoped>
+.listActions {
+    display: flex;
+    gap: 1.2rem;
+    justify-content: end;
+}
+
+.listActions__remove {
+    width: fit-content;
+    align-self: end;
+}
+</style>
