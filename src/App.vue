6c0c1d6dd31ada91ff1278d30430e962a7e1b01a
Pierre Charpentier <charpentier.pierre@outlook.com>
2024-03-13 23:25:48 +0100

    feat(ESLint): apply ESLint

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,20 @@ import Preview from './components/Preview.vue'
 </script>
 
 <template>
-    <section class="app">
-        <Editor class="app__section" />
-        <Preview class="app__section" />
-    </section>
+  <section class="app">
+    <Editor class="app__section" />
+    <Preview class="app__section" />
+  </section>
 </template>
 
 <style scoped>
 .app {
-    height: 100%;
-    display: flex;
+  height: 100%;
+  display: flex;
 }
 
 .app__section {
-    flex: 1;
-    height: 100%;
+  flex: 1;
+  height: 100%;
 }
 </style>
