196b99c9ae35656e1d5c5320e14f6e38808a6234
Pierre Charpentier <charpentier.pierre@outlook.com>
2024-03-14 09:27:48 +0100

    style(Prettier): format all files with Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import Editor from './components/Editor.vue'
-import Preview from './components/Preview.vue'
+import Editor from "./components/Editor.vue";
+import Preview from "./components/Preview.vue";
 </script>
 
 <template>
