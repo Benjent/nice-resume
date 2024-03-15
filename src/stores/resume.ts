f591889120b855ef9a113c9a131425687ccee7c6
Benjent <Benjent@hotmail.fr>
2024-03-15 22:13:05 +0100

    feat: allow section title edition

--- src/stores/resume.ts
+++ src/stores/resume.ts
@@ -16,12 +16,15 @@ export const useResumeStore = defineStore("resume", () => {
   const website = ref("");
 
   const workExperience = ref<WorkExperience[]>([]);
+  const workExperienceLabel = ref("Work experience");
   const education = ref<Education[]>([]);
+  const educationLabel = ref("Education");
 
   return {
     address,
     drivingLicense,
     education,
+    educationLabel,
     email,
     gitHub,
     linkedIn,
@@ -30,5 +33,6 @@ export const useResumeStore = defineStore("resume", () => {
     title,
     website,
     workExperience,
+    workExperienceLabel,
   };
 });
