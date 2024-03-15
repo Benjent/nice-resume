ac4a8fa16a664bfa0921197b60f96e4773d1bfeb
Benjent <Benjent@hotmail.fr>
2024-03-15 22:13:05 +0100

    feat: allow section title edition

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,12 +1,8 @@
-<script setup lang="ts">
-import { defineProps } from "vue";
-
-defineProps<{ heading: string }>();
-</script>
-
 <template>
-    <details class="flex flex-col p-8">
-        <summary class="cursor-pointer border-b-2 pb-2 mb-2">{{ heading }}</summary>
-        <slot />
-    </details>
+  <details class="flex flex-col p-8">
+    <summary class="cursor-pointer border-b-2 pb-2 mb-2">
+      <slot name="header" />
+    </summary>
+    <slot />
+  </details>
 </template>
