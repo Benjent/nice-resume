03bc82980af109ae32aa7c22ee64d26bea64721e
Benjent <Benjent@hotmail.fr>
2024-03-17 18:05:46 +0100

    feat(Editor.vue): add h1 for ally

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -61,6 +61,7 @@ function addTraining() {
 
 <template>
   <main class="flex flex-col overflow-y-scroll">
+    <h1 class="text-center text-xl p-6">Nice Resume</h1>
     <Category class="w-full">
       <template v-slot:header> Personal details </template>
       <div class="flex flex-col gap-5">
