8434a14a19e77d54c9c35953b6ded5fbc5711d87
Benjent <Benjent@hotmail.fr>
2024-03-19 17:41:06 +0100

    refactor(styles): use single POE for style import in maint.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import "./styles/tailwind.css";
-import "./styles/style.css";
+import "./styles/index.css";
 import App from "./App.vue";
 
 const pinia = createPinia();
