4625a70e2502e3c8afa46b7fb760767eeeaf61ab
Benjent <Benjent@hotmail.fr>
2024-03-23 21:37:25 +0100

    chore: retrieve components from current repo

---
+++ components/Category.vue
@@ -0,0 +1,8 @@
+<template>
+  <details class="flex flex-col p-8">
+    <summary class="cursor-pointer border-b-2 pb-2 mb-2">
+      <slot name="header" />
+    </summary>
+    <slot />
+  </details>
+</template>
