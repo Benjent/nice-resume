b33d4e35a9eb080858b949fe1b70cd0ac5f377d7
Franck Kerbiriou <FranckKe@users.noreply.github.com>
2024-03-23 23:27:47 +0100

    Nuxt from scratch (#27)
    * Nuxt Tailwind
    
    * Fix case
    
    ---------
    
    Co-authored-by: Benjent <Benjent@hotmail.fr>

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./styles/index.css";
-import App from "./App.vue";
+import App from "./app.vue";
 
 const pinia = createPinia();
 const app = createApp(App);
