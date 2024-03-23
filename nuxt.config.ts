e1690441679d5a11a751461d56843db6294d05c4
Benjent <Benjent@hotmail.fr>
2024-03-23 18:43:41 +0100

    fix(postcss): nuxtify postcss config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   typescript: {
     typeCheck: true,
   },
