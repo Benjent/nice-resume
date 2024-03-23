eb791737f29d4ab686f33155bdedaf812041c52a
Benjent <Benjent@hotmail.fr>
2024-03-23 22:02:59 +0100

    fix(nuxtconfig): provide pinia nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
   typescript: {
     typeCheck: true,
   },
