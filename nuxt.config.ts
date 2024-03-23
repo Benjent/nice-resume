7c4aaa622ba5d7d3188b08b99d2c511ecdaf96ec
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-03-24 00:06:18 +0100

    Fix a11y

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en-US",
+      },
+    },
+  },
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
   postcss: {
     plugins: {
