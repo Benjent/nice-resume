7911bdb667986604c4bd4ddbffdf35dce3226980
Benjent <Benjent@hotmail.fr>
2024-03-23 21:54:40 +0100

    chore: import seo metadata from current repo

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Editor from "./components/Editor.vue";
+import Preview from "./components/Preview.vue";
+
+const title = ref("Nice resume");
+</script>
+
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline text-green-500">Hello world!</h1>
-    <NuxtWelcome />
-  </div>
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="description" :content="title" />
+  </Head>
+  <section class="flex h-full bg-gradient-to-br from-blue-700 to-pink-500">
+    <Editor class="flex-1" />
+    <Preview id="preview" class="flex-1" />
+  </section>
 </template>
