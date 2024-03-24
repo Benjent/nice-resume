033797bf745185f0b8ff3b1f6268fe54910c05be
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-03-24 18:14:47 +0100

    Deploy, cicd, devcontainers, pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,13 @@ export default defineNuxtConfig({
   css: ["@/assets/styles/index.css"],
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "nuxt-svgo"],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en-US",
+      },
+    },
+  },
   typescript: {
     typeCheck: true,
   },
