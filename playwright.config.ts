d0914129ee8b501c0eb3903d78218747118b2a1c
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-04-07 01:05:22 +0200

    fix: e2e?

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 import type { ConfigOptions } from "@nuxt/test-utils/playwright";
 import dotenv from "dotenv";
 import path from "path";
@@ -18,10 +18,35 @@ console.log(`Base URL: ${process.env.BASE_URL}`);
 
 export default defineConfig<ConfigOptions>({
   fullyParallel: true,
+  testDir: "e2e",
   use: {
     nuxt: {
       rootDir: currentDir,
     },
     baseURL: process.env.BASE_URL || "http://localhost:3000",
   },
+  projects: [
+    /* Test against desktop browsers */
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    /* Test against mobile viewports. */
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
+  ],
 });
