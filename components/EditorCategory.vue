ddafa8c116b6ea8dd161aef8135bd514d1ba8e27
Benjent <benjent@hotmail.fr>
2024-04-06 22:24:32 +0300

    feat: make EditorCategory an accordion and remove no-check on a11y nested interactive behavior

--- components/EditorCategory.vue
+++ components/EditorCategory.vue
@@ -1,12 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/vue/24/outline";
+
+const isOpen = ref(true);
+
+function toggleDisplay() {
+  isOpen.value = !isOpen.value;
+}
+</script>
+
 <template>
-  <details class="flex flex-col bg-white bg-opacity-10 rounded">
-    <summary
-      class="cursor-pointer p-6 flex justify-between items-center rounded bg-white text-pink-500 shadow-lg"
+  <div class="flex flex-col bg-white bg-opacity-10 rounded">
+    <header
+      class="p-6 flex justify-between items-center rounded bg-white text-pink-500 shadow-lg"
     >
       <slot name="header" />
-    </summary>
-    <div class="p-6">
+      <button
+        title="Toggle display"
+        class="text-pink-500 size-7 rounded-full p-1 hover:bg-blue-700 hover:bg-opacity-5"
+        @click="toggleDisplay"
+      >
+        <ChevronUpIcon v-if="isOpen" class="size-full" />
+        <ChevronDownIcon v-else class="size-full" />
+      </button>
+    </header>
+    <div class="p-6" v-if="isOpen">
       <slot />
     </div>
-  </details>
+  </div>
 </template>
