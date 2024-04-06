bae27af3e49b6d64d67fa33d1bf0bd24d4983b37
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-04-07 00:18:07 +0200

    feat: center preview

--- components/Preview.vue
+++ components/Preview.vue
@@ -41,7 +41,7 @@ const component: Component = computed(() => templateMapper[template.value]);
 <template>
   <aside class="lg:overflow-y-auto">
     <section
-      class="w-[210mm] h-[297mm] bg-white shadow origin-top-left m-auto after:content-[''] after:block after:left-0 after:right-0 after:border-b-2 after:border-black after:border-dotted"
+      class="w-[210mm] h-[297mm] bg-white shadow origin-top-center m-auto after:content-[''] after:block after:left-0 after:right-0 after:border-b-2 after:border-black after:border-dotted"
       :style="`transform: scale(${zoomLevel / 100})`"
     >
       <component :is="component" />
