ecb4f503f88a6762f9abb365f49b7a6267d48e36
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-04-09 22:55:47 +0200

    feat(ui): persist store

--- stores/resume.ts
+++ stores/resume.ts
@@ -3,23 +3,27 @@ import { defineStore } from "pinia";
 import type { Category, Detail, Link, Template } from "@/types";
 import { templates } from "@/globals";
 
-export const useResumeStore = defineStore("resume", () => {
-  const template = ref<Template>(templates[0]);
+// @ts-expect-error - TS error with "state: () => {}" syntax
+export const useResumeStore = defineStore("resume", {
+  state: () => {
+    const template = ref<Template>(templates[0]);
 
-  const name = ref("");
-  const title = ref("");
-  const about = ref("");
-  const contactDetails = ref<Detail[]>([]);
-  const socialLinks = ref<Link[]>([]);
-  const categories = ref<Category[]>([]);
+    const name = ref("");
+    const title = ref("");
+    const about = ref("");
+    const contactDetails = ref<Detail[]>([]);
+    const socialLinks = ref<Link[]>([]);
+    const categories = ref<Category[]>([]);
 
-  return {
-    about,
-    categories,
-    contactDetails,
-    name,
-    socialLinks,
-    template,
-    title,
-  };
+    return {
+      about,
+      categories,
+      contactDetails,
+      name,
+      socialLinks,
+      template,
+      title,
+    };
+  },
+  persist: true,
 });
