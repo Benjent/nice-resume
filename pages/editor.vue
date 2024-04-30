80dd494ef5f7278b00e55d06d1a53fe57dfc94b4
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-04-30 19:56:46 +0200

    fix: wip resize

--- pages/editor.vue
+++ pages/editor.vue
@@ -7,8 +7,17 @@ import { Splitpanes, Pane } from "splitpanes";
 import "splitpanes/dist/splitpanes.css";
 
 const title = ref("Nice resume");
-</script>
+const todoSize = ref(40);
 
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+// @ts-disable-next-line no-explicit-any
+function foo(event: any) {
+  console.log(event[0].size);
+  todoSize.value =
+    (event as Array<any>)[0].size < 40 ? 0 : (event as Array<any>)[0].size;
+  console.log(event[0].size);
+}
+</script>
 <template>
   <Head>
     <Title>{{ title }}</Title>
@@ -18,11 +27,12 @@ const title = ref("Nice resume");
   <section
     class="flex flex-col bg-gradient-to-br from-blue-700 to-pink-500 lg:h-[calc(100svh-100px)] lg:flex-row"
   >
-    <splitpanes class="default-theme">
-      <pane>
+    <splitpanes class="default-theme" @resize="foo">
+      {{ todoSize }}
+      <pane :size="todoSize">
         <Editor class="flex-1" />
       </pane>
-      <pane>
+      <pane :size="100 - todoSize">
         <Preview />
       </pane>
     </splitpanes>
