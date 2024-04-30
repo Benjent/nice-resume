a4fa660dea3d9d03ef6941760d71b99cb96b2b3b
Franck Kerbiriou <contact@franckkerbiriou.com>
2024-04-30 20:29:22 +0200

    chore: responsive

--- pages/editor.vue
+++ pages/editor.vue
@@ -5,18 +5,10 @@ import Preview from "../components/Preview.vue";
 import TheHeader from "../components/TheHeader.vue";
 import { Splitpanes, Pane } from "splitpanes";
 import "splitpanes/dist/splitpanes.css";
+import { useMediaQuery } from "@vueuse/core";
 
 const title = ref("Nice resume");
-const todoSize = ref(40);
-
-/* eslint-disable  @typescript-eslint/no-explicit-any */
-// @ts-disable-next-line no-explicit-any
-function foo(event: any) {
-  console.log(event[0].size);
-  todoSize.value =
-    (event as Array<any>)[0].size < 40 ? 0 : (event as Array<any>)[0].size;
-  console.log(event[0].size);
-}
+const isSmallScreen = useMediaQuery("(max-width: 1024px)");
 </script>
 <template>
   <Head>
@@ -24,15 +16,17 @@ function foo(event: any) {
     <Meta name="description" :content="title" />
   </Head>
   <TheHeader />
-  <section
-    class="flex flex-col bg-gradient-to-br from-blue-700 to-pink-500 lg:h-[calc(100svh-100px)] lg:flex-row"
-  >
-    <splitpanes class="default-theme" @resize="foo">
-      {{ todoSize }}
-      <pane :size="todoSize">
+  <section class="bg-gradient-to-br from-blue-700 to-pink-500">
+    <splitpanes
+      class="default-theme flex flex-col lg:h-[calc(100svh-100px)] lg:flex-row"
+      :min="0"
+      :max="100"
+      :horizontal="isSmallScreen"
+    >
+      <pane min-size="50">
         <Editor class="flex-1" />
       </pane>
-      <pane :size="100 - todoSize">
+      <pane>
         <Preview />
       </pane>
     </splitpanes>
