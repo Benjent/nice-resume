70f4035909e2b88f88d1e35480264d6e10bf8128
Benjent <benjent@hotmail.fr>
2024-04-30 13:39:54 +0300

    feat(landing.vue): always allow redirection to main page on logo click

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -7,11 +7,13 @@
     <header
       class="sticky top-0 z-10 h-[100px] flex justify-between items-center gap-2 px-10 text-white border-white border-b-2 border-opacity-5"
     >
-      <h1 class="text-center text-4xl font-black tracking-widest uppercase">
-        Nice
-        <br />
-        Resume
-      </h1>
+      <NuxtLink to="/">
+        <h1 class="text-center text-4xl font-black tracking-widest uppercase">
+          Nice
+          <br />
+          Resume
+        </h1>
+      </NuxtLink>
     </header>
     <slot />
   </div>
