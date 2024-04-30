198f431c6d6ffbf94e192790cac6a775dd942cf6
Benjent <Benjent@hotmail.fr>
2024-04-30 12:41:26 +0200

    feat: use landing design to style error page (#63)
    * feat: use landing design to style error page
    
    * feat(pages/index.vue): set description meta and provide both title and description for opengraph
    
    * feat(landing.vue): always allow redirection to main page on logo click

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,45 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { ArrowLongRightIcon } from "@heroicons/vue/24/outline";
 import Button from "@/components/Button.vue";
+import Card from "@/components/Card.vue";
 
 const title = ref("Nice resume");
+const description = ref(
+  "Pick a design, build your resume, write your cover letter, adapt the design to your need with Nice Resume.",
+);
 </script>
 
 <template>
-  <div
-    class="bg-gradient-to-br from-blue-700 to-pink-500 backdrop-contrast-200 text-white"
-  >
-    <Head>
-      <Title>{{ title }}</Title>
-      <Meta name="description" :content="title" />
-    </Head>
-    <NuxtLayout>
-      <header
-        class="sticky top-0 z-10 h-[100px] flex justify-between items-center gap-2 px-10 text-white border-white border-b-2 border-opacity-5"
-      >
-        <h1 class="text-center text-4xl font-black tracking-widest uppercase">
-          Nice
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="description" :content="description" />
+    <Meta name="og:title" :content="title" />
+    <Meta name="og:description" :content="description" />
+    <!-- TODO og:url -->
+    <!-- TODO og:image -->
+  </Head>
+  <NuxtLayout name="landing">
+    <section class="p-20">
+      <Card>
+        <p class="text-4xl font-bold">
+          Pick a design
           <br />
-          Resume
-        </h1>
-      </header>
-      <section class="lg:h-[calc(100svh-100px)]">
-        <div
-          class="max-w-[800px] mx-auto my-20 p-20 bg-white bg-opacity-5 rounded-lg border-white border-2 border-opacity-20"
-        >
-          <p class="text-4xl font-bold">
-            Pick a design
-            <br />
-            Build your resume
-            <br />
-            Write your cover letter
-            <br />
-            Adapt the design to your need
-          </p>
-          <p class="text-2xl font-semibold mt-8">
-            It's
-            <i class="font-serif">that</i>
-            simple
-          </p>
-          <NuxtLink to="/editor">
-            <Button class="block mx-auto mt-8">Get started</Button>
-          </NuxtLink>
-        </div>
-        <div class="max-w-[800px] mx-auto my-20">
-          <p class="text-4xl font-bold font-serif italic">
-            Not your first time?
-          </p>
-          <NuxtLink to="/editor">
-            <Button class="mx-auto mt-8">
-              Continue where I left off
-              <ArrowLongRightIcon class="h-6 text-pink-500 float-right" />
-            </Button>
-          </NuxtLink>
-        </div>
-      </section>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+          Build your resume
+          <br />
+          Write your cover letter
+          <br />
+          Adapt the design to your need
+        </p>
+        <p class="text-2xl font-semibold mt-8">
+          It's
+          <i class="font-serif">that</i>
+          simple
+        </p>
+        <NuxtLink to="/editor">
+          <Button class="block mx-auto mt-8">Get started</Button>
+        </NuxtLink>
+      </Card>
+    </section>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
